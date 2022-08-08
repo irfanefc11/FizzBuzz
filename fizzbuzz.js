91326dd55355d1315ca3c45eccbf56b1c33d6b48
irfan151073 <irfan151073@gmail.com>
2022-08-08 11:42:05 +0700

    FizzBuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var multipleOf = function(factor, number) {
+  return number % factor === 0;
+};
+
+var mappings = [
+  { factor: 3, word: 'Fizz' },
+  { factor: 5, word: 'Buzz' }
+];
+
+var fizzbuzz = function(number) {
+  if ((typeof number) !== 'number') {
+    throw new TypeError('"' + number + '" is not a valid input');
+  }
+  var words = mappings
+    .filter(function(mapping) {
+      return multipleOf(mapping.factor, number);
+    })
+    .map(function(mapping) {
+      return mapping.word;
+    });
+  if (words.length > 0) {
+    return words.join('');
+  } else {
+    return number.toString();
+  }
+};
+
+module.exports = fizzbuzz;
